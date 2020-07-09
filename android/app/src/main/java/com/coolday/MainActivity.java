c5a6006b01831379a53bda6fdea8ddbca03f8329
WHTaki <47456924+WHTaki@users.noreply.github.com>
2020-07-09 18:11:54 +0800

    ety

---
+++ android/app/src/main/java/com/coolday/MainActivity.java
@@ -0,0 +1,25 @@
+package com.coolday;
+
+import android.os.Bundle;
+import android.content.Intent;
+import com.facebook.react.ReactActivity;
+//NativeModuleInjectionMark-mainActivity-import
+import org.devio.rn.splashscreen.SplashScreen;
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //NativeModuleInjectionMark-mainActivity-onCreate
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "CoolDay";
+  }
+}
